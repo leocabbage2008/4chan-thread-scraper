ef159f2e40e3c2ffea17b9b75803be57acb98f24
cplxily <70451398+leocabbage2008@users.noreply.github.com>
2024-07-16 21:11:45 -0400

    in case you do not have knowledge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import zipfile
 import requests
-board="b"
-thread="921442765"
+link="https://boards.4chan.org/b/thread/921442765"
+board=link.split("/thread")[0].split("/")[-1]
+thread=link.split("/")[-1]
 with zipfile.ZipFile('someZipFile.zip', 'w') as z: [z.writestr(i,requests.get(f"https://i.4cdn.org/{board}/{i}").content) for i in [str(i["tim"])+i["ext"] for i in requests.get(f"https://a.4cdn.org/{board}/thread/{thread}.json").json()["posts"] if "tim" in i.keys()]]
